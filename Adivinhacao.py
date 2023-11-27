b4887e94da14924218718ec7895c70914b19a422
eder-castro <132703429+eder-castro@users.noreply.github.com>
2023-11-27 20:55:12 -0300

    Atualizar o Adivinhacao.py Jogo com níveis de dificuldade

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,34 +1,49 @@
 import random
 
-print("***********************")
-print("*                     *")
-print("* Jogo de adivinhação *")
-print("*                     *")
-print("***********************")
+print ("***********************")
+print ("*                     *")
+print ("* Jogo de adivinhação *")
+print ("*                     *")
+print ("***********************")
 
-numero_secreto = 25
-chances = 3
+print("1 - FÁCIL - 10 chances")
+print("2 - MÉDIO - 06 chances")
+print("3 - DIFÍCIL - 03 chances")
+
+nivel = input("Escolha um nivel para jogar:")
+level = int (nivel)
+
+numero_secreto = round(random.randrange(1, 31))
+
+if (level == 1):
+    chances = 10
+elif (level == 2):
+        chances = 6
+else:
+        chances = 3
+        print (chances)
+
 rodada = 1
-while (rodada <= chances):
-    chutxt = input("Digite seu palpite (entre 1 e 30): ")
+while (rodada <= chances): 
+    print ("Rodada {} de {}".format(rodada, chances))
+    chutxt = input ("Digite seu palpite (entre 1 e 30): ")
     if (chutxt == ""):
-        print("ATENÇÃO!!! Voce deve digitar um.palpite!")
-        chutxt = input("Digite seu palpite (entre 1 e 30): ")
+        print ("ATENÇÃO!!! Voce deve digitar um.palpite!")
+        continue
     else:
-        chute = int(chutxt)
+            chute = int(chutxt)
     if (chute > 30 or chute < 1):
-        print("ATENÇÃO!!! Seu palpite deve estar entre 1 e 30")
+        print ("ATENÇÃO!!! Seu palpite deve estar entre 1 e 30")
     else:
-        print("Rodada {} de {}".format(rodada, chances))
         if (chute == numero_secreto):
-            print("Acertou, Mizeravi!")
-            exit()
+          print ("Acertou, Mizeravi!")
+          break
         else:
             if (chute > numero_secreto):
-                print("Seu chute foi maior!")
+              print ("Seu chute foi maior!")
             elif (chute < numero_secreto):
-                print("Seu chute foi menor!")
-        rodada = rodada + 1
-
-print("Voce errou as 3 tentativas")
-print("Jogo finalizado")
+              print ("Seu chute foi menor!")
+        rodada = rodada + 1  
+      
+print ("Jogo finalizado")
+  
