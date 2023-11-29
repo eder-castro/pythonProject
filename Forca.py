377b6d5759f9206ccd55c555d7482e837866b94b
eder-castro <132703429+eder-castro@users.noreply.github.com>
2023-11-29 00:04:41 -0300

    CRIACAO DO MENU DE JOGOS E JOGO DA FORCA

---
+++ Forca.py
@@ -0,0 +1,12 @@
+def jogar():
+    print("*****************************************")
+    print("*                                       *")
+    print("*             Jogo da Forca             *")
+    print("*                                       *")
+    print("*****************************************","\n")
+
+    print("Jogando Forca")
+    print("Jogo finalizado")
+
+if (__name__ == "__main__"):
+    jogar()
