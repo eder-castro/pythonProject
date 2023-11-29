aa318c862d94a188fe71746219fcd27f047c86be
eder-castro <132703429+eder-castro@users.noreply.github.com>
2023-11-28 23:09:32 -0300

    Adicionado comentario com a data

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,50 +1,54 @@
--- 27/11/2023
 import random
 
-print ("***********************")
-print ("*                     *")
-print ("* Jogo de adivinhação *")
-print ("*                     *")
-print ("***********************")
+-- 27 / 11 / 2023
+
+print("*****************************************")
+print("*                                       *")
+print("*          Jogo de adivinhação          *")
+print("*                                       *")
+print("*****************************************","\n")
 
 print("1 - FÁCIL - 10 chances")
 print("2 - MÉDIO - 06 chances")
-print("3 - DIFÍCIL - 03 chances")
+print("3 - DIFÍCIL - 03 chances","\n")
 
-nivel = input("Escolha um nivel para jogar:")
-level = int (nivel)
-
+nivel = input("Escolha um nível para jogar: ")
+level = int(nivel)
+rodada = 1
 numero_secreto = round(random.randrange(1, 31))
+pontos = 100
 
 if (level == 1):
     chances = 10
 elif (level == 2):
-        chances = 6
+    chances = 6
 else:
-        chances = 3
-        print (chances)
+    chances = 3
 
-rodada = 1
 while (rodada <= chances): 
-    print ("Rodada {} de {}".format(rodada, chances))
-    chutxt = input ("Digite seu palpite (entre 1 e 30): ")
+    print("Rodada {} de {}".format(rodada, chances))
+    chutxt = input("Digite seu palpite (entre 1 e 30): ")
     if (chutxt == ""):
-        print ("ATENÇÃO!!! Voce deve digitar um.palpite!")
+        print("ATENÇÃO!!! Você deve digitar um palpite!")
         continue
     else:
-            chute = int(chutxt)
+        chute = int(chutxt)
     if (chute > 30 or chute < 1):
-        print ("ATENÇÃO!!! Seu palpite deve estar entre 1 e 30")
+        print("ATENÇÃO!!! Seu palpite deve estar entre 1 e 30")
     else:
         if (chute == numero_secreto):
-          print ("Acertou, Mizeravi!")
-          break
+            print("Você acertou e fez {} pontos!".format(pontos))
+            break
         else:
             if (chute > numero_secreto):
-              print ("Seu chute foi maior!")
+                print("Seu chute foi maior!")
+                if (rodada == chances):
+                    print("O número secreto era {}. Você faria {} pontos se tivesse acertado".format(numero_secreto, pontos))
             elif (chute < numero_secreto):
-              print ("Seu chute foi menor!")
-        rodada = rodada + 1  
-      
-print ("Jogo finalizado")
-  
+                print("Seu chute foi menor!")
+                if (rodada == chances):
+                    print("O número secreto era {}. Você faria {} pontos se tivesse acertado".format(numero_secreto, pontos))
+        rodada = rodada + 1
+        pontos = pontos - abs(numero_secreto-chute)
+
+print("Jogo finalizado")
