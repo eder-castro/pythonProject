58b91a12ed2ff9ce4c4c3ea6c8e2ca9c586eca57
eder-castro <132703429+eder-castro@users.noreply.github.com>
2023-12-01 17:59:15 -0300

    Atualizar o Forca.py
    Jogo atualizado até "Escrever as letras nas posições corretas e mostrar em tela"

--- Forca.py
+++ Forca.py
@@ -5,6 +5,25 @@ def jogar():
     print("*                                       *")
     print("*****************************************","\n")
 
+    palavra_secreta = "abacaxi"
+    acertou = False
+    enforcado = False
+    letras_certas = ["_",'_',"_","_","_","_","_"]
+    
+    print(letras_certas)
+  
+    while (not acertou and not enforcado):
+        chute = input("Qual a letra ? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+                letras_certas[index] = letra
+            index = index + 1
+
+        print(letras_certas)
+
     print("Jogando Forca")
     print("Jogo finalizado")
 
